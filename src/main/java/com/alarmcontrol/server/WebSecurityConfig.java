2be50b2222db31a3bd9b2d3eeff6b851004e970e
Dennis Kniep <kniepdennis@gmail.com>
2019-06-24 18:50:51 +0200

    Remove cors exception for development

--- src/main/java/com/alarmcontrol/server/WebSecurityConfig.java
+++ src/main/java/com/alarmcontrol/server/WebSecurityConfig.java
@@ -1,19 +1,13 @@
 package com.alarmcontrol.server;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration()
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Value("${cors.allow-all}")
-  private boolean corsAllowAll;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -21,11 +15,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/**").permitAll();
 
     http.antMatcher("/**").csrf().disable();
-    http.antMatcher("/**").headers().frameOptions().disable();
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-    http.cors().configurationSource(source);
   }
-
 }
