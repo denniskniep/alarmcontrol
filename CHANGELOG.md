d5d9db75ef8dfc644ebd4762802c7acbdee18ab2
Dennis Kniep <kniepdennis@gmail.com>
2019-08-04 22:38:33 +0200

    Optimize common employee status view on home site for more employees

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## Added
 ## Changed
 ## Fixed
+- Optimize common employee status view for more employees
+
 
 ## [1.1.0] - 2019-08-03
 ### Added
--- frontend/src/home/employeeStatusAsList.js
+++ frontend/src/home/employeeStatusAsList.js
@@ -2,10 +2,56 @@ import {Col, Row, Table} from "react-bootstrap";
 import React, {Component} from 'react';
 import PrettyPrinter from "../utils/prettyPrinter";
 import EmployeeStatusDot from "./employeeStatusDot";
+import EmployeeStates from "./employeeStates";
 
 class EmployeeStatusAsList extends Component {
 
+  firstCharWithDot(value){
+    if(value && value.length > 0){
+      return value[0] + "."
+    }
+    return null;
+  }
+
+  renderEmployee(e){
+    return (
+        <tr key={e.id}>
+          <td className={"dot-td employee-status-td"}>
+            <p className={"dot-td-container"}>
+              <EmployeeStatusDot employee={e} />
+            </p>
+          </td>
+          <td className={"employee-status-td"}>
+            <p className={"employee-status-name"}>
+              {this.firstCharWithDot(e.firstname)} {e.lastname}
+            </p>
+            <p className={"employee-status-date"}>
+              {e.status
+              && new PrettyPrinter().prettifyDateTimeShort(
+                  e.status.dateTime)}
+            </p>
+          </td>
+        </tr>
+    )
+  }
+
+  mapStatusToIntForOrder(employee){
+    if(!employee.status){
+      return 2;
+    }
+
+    if(employee.status.status == EmployeeStates.getNotAvailable()){
+      return 1;
+    }
+
+    if(employee.status.status == EmployeeStates.getAvailable()){
+      return 0;
+    }
+  }
+
   render() {
+    let employees = this.props.employees.sort((a, b) => this.mapStatusToIntForOrder(a) - this.mapStatusToIntForOrder(b));
+
     return (
         <React.Fragment>
           <Row>
@@ -13,28 +59,18 @@ class EmployeeStatusAsList extends Component {
               <Table responsive>
                 <tbody>
                 {
-                  this.props.employees.map(
-                      (e, index) => {
-                        return (
-                            <tr key={e.id}>
-                              <td className={"dot-td"}>
-                                <p className={"dot-td-container"}>
-                                  <EmployeeStatusDot employee={e} />
-                                </p>
-                              </td>
-                              <td>
-                                <p className={"employee-status-name"}>
-                                  {e.firstname} {e.lastname}
-                                </p>
-                                <p className={"employee-status-date"}>
-                                  {e.status
-                                  && new PrettyPrinter().prettifyDateTimeShort(
-                                      e.status.dateTime)}
-                                </p>
-                              </td>
-                            </tr>
-                        )
-                      })
+                  employees.map((e, index) =>
+                      index % 2 == 0 && this.renderEmployee(e))
+                }
+                </tbody>
+              </Table>
+            </Col>
+            <Col className={"noPadding"}>
+              <Table responsive>
+                <tbody>
+                {
+                  employees.map((e, index) =>
+                      index % 2 == 1  && this.renderEmployee(e))
                 }
                 </tbody>
               </Table>
