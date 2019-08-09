708fee8297d963fb719c58467e2346b7bc85c397
Dennis Kniep <kniepdennis@gmail.com>
2019-08-09 20:34:58 +0200

    Fix menu entry if no keyword and address is present

--- frontend/src/menu.js
+++ frontend/src/menu.js
@@ -51,10 +51,14 @@ class Menu extends Component {
 
   getMenuTitle(alert) {
     let keyword = alert.keyword ? alert.keyword : "";
-    let address = alert.addressInfo1 ? " - " + alert.addressInfo1 : "";
+    let address = alert.addressInfo1 ? alert.addressInfo1 : "";
     let date = new PrettyPrinter().prettifyDateTimeLong(alert.dateTime);
     if (!alert.keyword && !alert.addressInfo1) {
       return date;
+    }
+
+    if(keyword && address){
+      address = " - " + address;
     }
 
     return keyword + address + " (" +  date + ")";
