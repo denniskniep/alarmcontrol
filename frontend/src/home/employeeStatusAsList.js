37655af06e448a14361819a81f8b99b0dd98fe9f
Dennis Kniep <kniepdennis@gmail.com>
2019-08-09 20:28:31 +0200

    Optimize common employee status view on home site for more employees -> use badges as in alert view

--- frontend/src/home/employeeStatusAsList.js
+++ frontend/src/home/employeeStatusAsList.js
@@ -1,77 +1,49 @@
-import {Col, Row, Table} from "react-bootstrap";
+import {Badge, Col, Row, Table} from "react-bootstrap";
 import React, {Component} from 'react';
-import PrettyPrinter from "../utils/prettyPrinter";
 import EmployeeStatusDot from "./employeeStatusDot";
 import EmployeeStates from "./employeeStates";
-import Card from "react-bootstrap/Card";
 
 class EmployeeStatusAsList extends Component {
 
-  firstCharWithDot(value){
-    if(value && value.length > 0){
-      return value[0] + "."
-    }
-    return null;
-  }
-
-  renderEmployee(e){
-
-    let color = null;
-    let status = e.status && e.status.status
-
-    switch (status) {
-      case EmployeeStates.getNotAvailable():
-        color= "#dc35458f";
-        break;
-      case EmployeeStates.getAvailable():
-        color= "#28a74559";
-        break;
-    }
-
-    return (
-        <Card style={{ width: '160px', height: '80px', backgroundColor: color }}>
-          <Card.Body>
-            <Card.Text>
-              <p className={"employee-status-name"}>
-                {this.firstCharWithDot(e.firstname)} {e.lastname}
-              </p>
-              <p className={"employee-status-date"}>
-                {e.status
-                && new PrettyPrinter().prettifyDateTimeShort(
-                    e.status.dateTime)}
-              </p>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-    )
-  }
-
-  mapStatusToIntForOrder(employee){
-    if(!employee.status){
+  mapStatusToIntForOrder(employee) {
+    if (!employee.status) {
       return 2;
     }
 
-    if(employee.status.status == EmployeeStates.getNotAvailable()){
+    if (employee.status.status == EmployeeStates.getNotAvailable()) {
       return 1;
     }
 
-    if(employee.status.status == EmployeeStates.getAvailable()){
+    if (employee.status.status == EmployeeStates.getAvailable()) {
       return 0;
     }
   }
 
   render() {
-    let employees = this.props.employees.sort((a, b) => this.mapStatusToIntForOrder(a) - this.mapStatusToIntForOrder(b));
+    let employees = this.props.employees.sort(
+        (a, b) => this.mapStatusToIntForOrder(a) - this.mapStatusToIntForOrder(
+            b));
 
     return (
         <React.Fragment>
-          <Row className={"noPadding h-100"}>
-            <Col className={"noPadding h-100"}>
-              <div className="d-flex flex-wrap flex-column h-100">
-                {
-                  employees.map((e, index) => this.renderEmployee(e))
-                }
-              </div>
+          <Row className={" h-100"}>
+            <Col className={" h-100"}>
+              {
+                employees.map((e, index) => {
+                  return (
+                    <Badge key={e.id} className={"badgeSpace badgeEmployee"}
+                           variant={"light"}>
+                      {
+                        e.status &&
+                        <EmployeeStatusDot className={"dot-sm dot-space"}
+                                           employee={e}/>
+                      }
+                      <span>
+                        {e.firstname} {e.lastname}
+                      </span>
+                    </Badge>)
+                })
+              }
             </Col>
           </Row>
         </React.Fragment>);
